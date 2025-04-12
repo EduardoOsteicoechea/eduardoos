b3d7156cd04c1c9679fab28e091ec95ea64ff394
eduardo <eduardooost@gmail.com>
2025-04-12 08:44:42 -0400

    testing deploy

--- api/Program.cs
+++ api/Program.cs
@@ -1,10 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ListenAnyIP(5000);
-});
-
 var app = builder.Build();
 
 app.MapGet("/api", () => "Hello World!");
